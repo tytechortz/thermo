9e7efdc4840c8b086b56116a7a7237afcd9b3600
James <tytechortz@gmail.com>
2022-12-31 15:52:15 -0700

    formats current temp

--- app.py
+++ app.py
@@ -2,17 +2,45 @@ import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output, State
 
+import requests
+
+url = "http://10.0.1.7:8080"
+
 
 # Build Dash layout
 app = dash.Dash(__name__)
 
 app.layout=html.Div([
+    html.Div(id='live-thermometer', style={'color':'green', 'font-size': 25, 'font-family':'sans-serif', 'text-align':'center'}),
     html.Div([
-        html.H2('Sup')
-    ],
-        className='three columns pretty_container'
-    ),
+        dcc.Interval(
+            id='interval-component-graph',
+            interval=900000,
+            n_intervals=0
+        ),
+        dcc.Interval(
+            id='interval-component',
+            interval=60000,
+            n_intervals=0
+        ),
+    ]),
 ])
+
+@app.callback(Output('live-thermometer', 'children'),
+              Input('interval-component', 'n_intervals'))
+def update_layout(n):
+    res = requests.get(url)
+    data = res.json()
+    f = ((9.0/5.0) * data) + 32
+
+    return html.Div([
+            html.H6('Current Temp', style={'text-align':'center'}),
+            html.H6('{}'.format(f), style={'text-align':'center'})
+        ],
+            className='three columns pretty_container'
+        )
+        
+    
 
 
 
